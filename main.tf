8e93301a309192cf29545aa6764be762056fa75b
seemagawade01 <52858990+seemagawade01@users.noreply.github.com>
2023-09-28 16:12:58 +0530

    api enabled and VM creation

---
+++ main.tf
@@ -0,0 +1,67 @@
+locals {
+
+  project_id = "groovy-height-400408"
+
+}
+
+ 
+
+provider "google" {
+
+  project = local.project_id
+
+  region  = "us-central1"
+
+  zone    = "us-central1-b"
+
+}
+
+ 
+
+resource "google_project_service" "compute_service" {
+
+  project = local.project_id
+
+  service = "compute.googleapis.com"
+
+}
+
+resource "google_compute_instance" "vm_instance" {
+
+  name         = "nginx-instance"
+
+  machine_type = "f1-micro"
+
+ 
+
+  tags = ["nginx-instance"]
+
+ 
+
+  boot_disk {
+
+    initialize_params {
+
+      image = "centos-7-v20230912"
+
+    }
+
+}
+
+  network_interface {
+
+    network = "default"
+
+    //access_config {
+
+      // The "0.0.0.0/0" here allows access from any IP. Consider restricting access for security.
+
+      //nat_ip = google_compute_address.static_ip.address
+
+    //}
+
+  }
+
+ 
+
+}
