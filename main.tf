99fd8c038c5dd9ab7ef8ae2dba0c1a866d7641d1
seemagawade01 <52858990+seemagawade01@users.noreply.github.com>
2024-02-02 19:16:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,6 @@ locals {
   project_id = "ascendant-quest-400509"
 }
 
-/*provider "google" {
-  credentials = file("serviceaccount.json")
-  project = local.project_id
-  region  = "us-central1"
-  zone    = "us-central1-b"
-}*/
-
 resource "google_project_service" "compute_service" {
   project = local.project_id
   service = "compute.googleapis.com"
