6ac9e93758c65305e7c1516c664ec873d27fe093
seemagawade01 <52858990+seemagawade01@users.noreply.github.com>
2024-02-02 17:17:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,6 +14,11 @@ resource "google_project_service" "compute_service" {
   service = "compute.googleapis.com"
 }
 
+resource "google_project_service" "cloudresourcemanager_service" {
+  project = local.project_id
+  service = "cloudresourcemanager.googleapis.com"
+}
+
 resource "google_compute_instance" "vm_instance" {
   name         = "nginx-instance"
   machine_type = var.linux_instance_type
