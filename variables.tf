611638d12c07a14e1a9a386c61137d3bfb982a61
seemagawade01 <52858990+seemagawade01@users.noreply.github.com>
2024-02-02 17:15:24 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,6 +7,5 @@ variable "linux_instance_type" {
 variable "cis-rhel-8-lvl-1" {
   type        = string
   description = "Red Hat Enterprise Linux 8 level 1"
-  #default     = "cis-red-hat-enterprise-linux-8-level-1-v2-0-0-9"
-  default     ="https://www.googleapis.com/compute/v1/projects/cis-public/global/images/cis-red-hat-enterprise-linux-8-level-1-v2-0-0-9"
+  default     = "https://www.googleapis.com/compute/v1/projects/cis-public/global/images/cis-red-hat-enterprise-linux-8-level-1-v2-0-0-12"
 }
