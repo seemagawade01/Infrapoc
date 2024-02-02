7dfbd902cd58d24c420a81e82cffa15e28eef258
seemagawade01 <52858990+seemagawade01@users.noreply.github.com>
2024-02-02 16:40:16 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "linux_instance_type" {
+  type        = string
+  description = "VM instance type for Linux Server"
+  default     = "n1-standard-2"  #n1-standard-2 (2 vCPU, 7.5 GB memory)
+}
