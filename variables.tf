51338ea2413e381687ce12ac3d682c2c65074f3e
seemagawade01 <52858990+seemagawade01@users.noreply.github.com>
2024-02-02 16:46:02 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,3 +3,10 @@ variable "linux_instance_type" {
   description = "VM instance type for Linux Server"
   default     = "n1-standard-2"  #n1-standard-2 (2 vCPU, 7.5 GB memory)
 }
+
+variable "cis-rhel-8-lvl-1" {
+  type        = string
+  description = "Red Hat Enterprise Linux 8 level 1"
+  #default     = "cis-red-hat-enterprise-linux-8-level-1-v2-0-0-9"
+  default     ="https://www.googleapis.com/compute/v1/projects/cis-public/global/images/cis-red-hat-enterprise-linux-8-level-1-v2-0-0-9"
+}
