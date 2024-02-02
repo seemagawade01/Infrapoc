4731971f80f087eb26cc6e5e7614a64500a26a00
seemagawade01 <52858990+seemagawade01@users.noreply.github.com>
2024-02-02 16:48:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,68 +1,31 @@
 locals {
-
-  project_id = "groovy-height-400408"
-
+  project_id = "ascendant-quest-400509"
 }
 
- 
-
-provider "google" {
+/*provider "google" {
   credentials = file("serviceaccount.json")
-
   project = local.project_id
-
   region  = "us-central1"
-
   zone    = "us-central1-b"
-
-}
-
- 
+}*/
 
 resource "google_project_service" "compute_service" {
-
   project = local.project_id
-
   service = "compute.googleapis.com"
-
 }
 
 resource "google_compute_instance" "vm_instance" {
-
   name         = "nginx-instance"
-
-  machine_type = "f1-micro"
-
- 
-
+  machine_type = var.linux_instance_type
   tags = ["nginx-instance"]
-
- 
-
   boot_disk {
-
     initialize_params {
-
-      image = "centos-7-v20230912"
-
+      image = var.cis-rhel-8-lvl-1
     }
-
 }
 
   network_interface {
-
     network = "default"
-
-    //access_config {
-
-      // The "0.0.0.0/0" here allows access from any IP. Consider restricting access for security.
-
-      //nat_ip = google_compute_address.static_ip.address
-
-    //}
-
-  }
-
- 
+} 
 
 }
